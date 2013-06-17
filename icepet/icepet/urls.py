0a14265bb794712236b13d881b06719207178a46
Helgi Hrafn Gunnarsson <helgi@binary.is>
2013-06-17 23:12:54 +0000

    Fix missing comma and merge problems

--- icepet/icepet/urls.py
+++ icepet/icepet/urls.py
@@ -13,11 +13,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-<<<<<<< HEAD
-    # url(r'^admin/', include(admin.site.urls)),
 
     url(r'^althingi/', include('althingi.urls')),
-=======
     url(r'^admin/', include(admin.site.urls)),
->>>>>>> 59de481a414a0579484aeb2ec61aa24fa3313d54
 )
