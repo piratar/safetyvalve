830753640cd1b87f5f528c78a68e70f77c5d2083
James Robb <james@greenqloud.com>
2013-06-29 23:56:54 +0000

    updated settings file so certain vars are actually overridden by local settings. updated local settings template

--- icepet/icepet/settings.py
+++ icepet/icepet/settings.py
@@ -3,6 +3,22 @@
 import os
 here = lambda x: os.path.realpath(os.path.join(os.path.realpath(os.path.dirname(__file__)), x))
 
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# In a Windows environment this must be set to your system time zone.
+TIME_ZONE = 'Iceland'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+#date format
+DATE_FORMAT = 'd-F-Y'
+
+# Moved the two vars above time_zone and language_code because otherwise they would not be
+# overwritten in the local settings import
+
 try:
     from local_settings import *
 except ImportError:
@@ -31,16 +47,6 @@ DATABASES = {
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
-
 SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
@@ -56,12 +62,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = here('uploads/')
+MEDIA_ROOT = here('../uploads/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = '../uploads/'
+MEDIA_URL = '/uploads/'
 
 # Absolute path to the directory static file\s should be collected to.
 # Don't put anything in this directory yourself; store your static files
