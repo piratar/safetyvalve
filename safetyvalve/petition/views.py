86e22dc231614c014f1addfc8b51b51c93d2c83b
Tómas Árni Jónasson <tomas@clara.is>
2013-07-03 22:04:19 +0000

    PEP8 fix petition/views.py

--- safetyvalve/petition/views.py
+++ safetyvalve/petition/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+
+import urllib
+
 from django.db.models import Count
 from django.template import Context
 from django.shortcuts import render
@@ -6,7 +8,6 @@ from django.conf import settings
 
 from petition.models import Petition
 
-import urllib
 
 def index(request):
     p = Petition.objects.all().order_by('-date_created').annotate(num_signatures=Count('signature'))[:5]
@@ -15,21 +16,22 @@ def index(request):
         if isinstance(i.name, unicode):
             i.name = i.name.encode('utf-8')
 
-        url = urllib.quote("%s/petition/%s" % (settings.INSTANCE_URL,str(i.id)))
+        url = urllib.quote("%s/petition/%s" % (settings.INSTANCE_URL, str(i.id)))
 
         #facebook share logic
         i.url_facebook_share = 'http://www.facebook.com/sharer/sharer.php?u=%s' % url
 
         #twitter length logic
         url_twitter_share = ('%s %s' % (url, i.name))
-        url_twitter_share = url_twitter_share if len(url_twitter_share) <= 140 else url_twitter_share[:137]+'...'
+        url_twitter_share = url_twitter_share if len(url_twitter_share) <= 140 else url_twitter_share[:137] + '...'
         i.url_twitter_share = 'http://twitter.com/home?status=' + urllib.quote(url_twitter_share)
 
         #g+ share logic
         i.url_googleplus_share = 'https://plus.google.com/share?url=%s' % url
 
-    context = Context({'petitions':p, 'instance_url':settings.INSTANCE_URL})
+    context = Context({'petitions': p, 'instance_url': settings.INSTANCE_URL})
     return render(request, 'index.html', context)
+
 
 def detail(request, petition_id):
     p = Petition.objects.get(id=petition_id)
