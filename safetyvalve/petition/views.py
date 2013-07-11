644a1c84e8aecb8a8d259a22a8451124d5774a78
Tómas Árni Jónasson <tomas@clara.is>
2013-07-11 00:03:29 +0000

    Nudgin authentication in place (some SOAP/SAML action)

--- safetyvalve/petition/views.py
+++ safetyvalve/petition/views.py
@@ -1,11 +1,12 @@
 
 from datetime import datetime
+from pprint import pprint
+from pysimplesoap.client import SoapClient
 from urllib import urlencode
 import urllib
 
 from django.db.models import Count, Q
 from django.conf import settings
-from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
@@ -51,18 +52,69 @@ def now():
     return datetime.now()
 
 
-@login_required
 def sign(request, petition_id):
     c = {}
 
     p = get_object_or_404(Petition, pk=petition_id)
-    user = request.user
 
-    auths = UserAuthentication.objects.filter(Q(expires=None) | Q(expires__gt=now()), user=user)
+    user = request.user
+    if user.is_authenticated():
+        auths = UserAuthentication.objects.filter(Q(expires=None) | Q(expires__gt=now()), user=user, petition=p)
+    else:
+        auths = []
 
     if not auths:
-        params = {'path': '%sauth/' % reverse('sign', args=(petition_id, ))[1:]}
-        return HttpResponseRedirect(settings.AUTH_URL % urlencode(params))
+
+        token = request.GET.get('token')
+
+        if not token:
+
+            params = {'path': reverse('sign', args=(petition_id, ))[1:]}
+            return HttpResponseRedirect(settings.AUTH_URL % urlencode(params))
+
+        else:
+
+            # Fetch SAML info
+            import base64
+
+            AI = settings.AUTH_ISLAND
+            encoded = base64.encodestring('%s:%s' % (AI['login'], AI['password']))
+            headers = {'Authorization': 'Basic %s' % encoded}
+
+            client = SoapClient(wsdl=AI['wsdl'], trace=False, http_headers=headers)
+
+            ipaddr = request.META.get('REMOTE_ADDR')
+#            ipaddr = '81.15.30.31'
+
+            result = client.generateSAMLFromToken(token, ipaddr)
+
+#            result_example = {
+#                'status': {
+#                    'message': 'Success',
+#                    'code': '',
+#                    'type': '',
+#                    #...
+#                },
+#                'saml': '<hello>SamlInfo</hello>',
+#            }
+
+            pprint(result['saml'])
+            saml = {
+                'kennitala': '1234',
+            }
+
+            if not user.is_authenticated():
+                user = User()
+                user.username = saml['kennitala']
+
+            auth = UserAuthentication()
+            auth.user = user
+            auth.petition = p
+            auth.token = token
+            auth.generated = datetime.now()
+            auth.method = 'icekey'
+            auth.save()
+
 
     s, created = Signature.objects.get_or_create(user=user, petition=p)
 
