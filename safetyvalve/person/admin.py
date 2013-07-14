3c1dae74b09f81097457a65f73d6dba9c8c7fcd2
Tómas Árni Jónasson <tomas@clara.is>
2013-07-14 17:33:40 +0000

    Adding admin for person app

---
+++ safetyvalve/person/admin.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from models import UserAuthentication
+
+
+class UserAuthenticationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'method', 'token', 'generated')
+
+
+admin.site.register(UserAuthentication, UserAuthenticationAdmin)
