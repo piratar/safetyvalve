fd3b8d01a011f9e32a9320f5fba63ce9c8a07fbd
Helgi Hrafn Gunnarsson <helgi@binary.is>
2013-08-25 17:40:42 +0000

    Central sign/unsign buttons for inclusion in other pages, including "show_public" feature

--- safetyvalve/petition/static/petition/js/petition.js
+++ safetyvalve/petition/static/petition/js/petition.js
@@ -6,6 +6,42 @@ $(function () {
             e.preventDefault();
             return false;
         }
-    })
+    });
+
+    $('.pre_selection_container a.addremove_signature').bind('click', function(e) {
+        container = $(this).parent().parent();
+
+        container.find('.pre_selection_container').hide();
+        container.find('.post_selection_container').show();
+
+        e.preventDefault();
+        return false;
+    });
+
+    $('.post_selection_container a.addremove_signature').bind('click', function(e) {
+        container = $(this).parent().parent();
+
+        show_public = container.find('.show_public').is(':checked');
+
+        petition_id = container.data('id');
+
+        url = '/petition/' + petition_id + '/sign/?show_public=' + (show_public ? '1' : '0');
+
+        location.href = url;
+
+        e.preventDefault();
+        return false;
+    });
+
+    $('.post_selection_container a.signature_cancel').bind('click', function(e) {
+        container = $(this).parent().parent();
+
+        container.find('.post_selection_container').hide();
+        container.find('.pre_selection_container').show();
+
+        e.preventDefault();
+        return false;
+    });
 
 });
+
