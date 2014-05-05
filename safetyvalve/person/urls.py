1a9180bd6c46f91af37826e13c86a9db57fb0c15
James Robb <james@gearedinteractive.ca>
2014-05-05 20:54:53 +0000

    basic my page functionality with new grid

--- safetyvalve/person/urls.py
+++ safetyvalve/person/urls.py
@@ -1,11 +1,14 @@
 
 from django.conf.urls import patterns, url
 
+from person import views
+
 
 urlpatterns = patterns('person.views',
     url(r'^innskra/$', 'login_view', name='login'),
     url(r'^utskra/$', 'logout_view', name='logout'),
-    url(r'^undirskriftir/$', 'signatures', name='signatures'),
+    url(r'^minsida/$', 'my_page', name='my_page'),
+    url(r'^minarundirskriftir/$', views.get_user_signatures, name='get_user_signatures'),
     url(r'^undirskriftir/fjarlaegja/(?P<signature_id>\d+)/$', 'remove_signature', name='remove_signature'),
     url(r'^undirskriftir/breyta-opinber/(?P<signature_id>\d+)/$', 'signature_change_public'),
 )
--- safetyvalve/person/views.py
+++ safetyvalve/person/views.py
@@ -1,8 +1,12 @@
 
+import json
+
+from math import floor
 from urllib import urlencode
 
 from django.conf import settings
 from django.contrib.auth import logout
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.urlresolvers import reverse
 from django.shortcuts import HttpResponse, HttpResponseRedirect
 from django.http import HttpResponseForbidden
@@ -13,23 +17,82 @@ from petition.models import Signature
 
 from icekey.utils import authenticate
 
+def get_user_signatures(request):
+
+    empty_response = False
+    response = []
+    total_signatures = 0
+
+    if request.user.is_authenticated():
+
+        start_index = int(request.GET.get('iDisplayStart', 0))
+        page_length = int(request.GET.get('iDisplayLength', 0))
+
+        if start_index < 0:
+            start_index = 0;
+        if page_length < 1:
+            page_length = 1
+
+        page_num = floor(start_index / page_length) + 1
+
+        sort_index = int(request.GET.get('iSortCol_0', 0))
+        sort_dir = request.GET.get('sSortDir_0', "desc")
+
+        if sort_dir not in ["asc", "desc"]:
+            sort_dir = "desc"
+
+        if sort_index == 0:
+            sort_fields = ['user__first_name', 'user__last_name']
+        else:
+            sort_fields = ['date_created']
+
+        for i in xrange(len(sort_fields)):
+            if sort_dir == "desc":
+                sort_fields[i] = "-"+sort_fields[i]
+
+        signatures = Signature.objects.select_related('petition__name').filter(user=request.user).order_by('-date_created')
+        print signatures
+
+        try:
+            p = Paginator(signatures, page_length)
+            results = p.page(page_num)
+        except PageNotAnInteger:
+            results = p.page(1)
+        except EmptyPage:
+            results = p.page(paginator.num_pages)
+
+        for s in results:
+            o = []
+            o.append(s.petition.name.capitalize())
+            o.append(s.date_created.strftime("%Y-%m-%d %H:%M:%S"))
+
+            response.append(o)
+
+        total_signatures = p.count
+
+    response_wrapper = {}
+    response_wrapper['iTotalRecords'] = total_signatures
+    response_wrapper['iTotalDisplayRecords'] = total_signatures
+    response_wrapper['aaData'] = response
+
+    return HttpResponse(json.dumps(response_wrapper), content_type="application/json")
+
 
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect('/')
 
-
-def signatures(request):
+def my_page(request):
     c = {}
 
     if not request.user.is_authenticated():
         return HttpResponseRedirect(reverse('login'))
 
-    signs = Signature.objects.filter(user=request.user).order_by('-date_created')
+    context = Context({
+        'signatures_url': settings.INSTANCE_URL + reverse('get_user_signatures') + ''  
+    })
 
-    c['signatures'] = signs
-
-    return render(request, 'petition/signatures.html', Context(c))
+    return render(request, 'person/my_page.html', context)
 
 
 def signature_change_public(request, signature_id):
